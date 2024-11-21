7938ef6049fdf6c49da3db7967db1d555c99d2e9
Nicoli Santos <nicolisantosbatista@gmail.com>
2024-11-21 17:13:11 -0300

    Estudo e atividades: for, funcao e while

---
+++ for_2.py
@@ -0,0 +1,4 @@
+#Coleção de números em uma lista
+n = [5,8,10,11,12,2,28,39,37,38,40]
+for i in n:
+    print(i,end=" ")
---
+++ valores.py
@@ -0,0 +1,6 @@
+x = []
+for x in range (1,21):
+    print(x)
+    
+for x in range (1,21):
+    print(x, end=" ")
